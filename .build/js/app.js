5eede57cc4e5a19b65c755b53954eed50f20a66b
Alan Denison <alan79td@gmail.com>
2016-08-05 15:54:12 -0700

    display results on page

--- .build/js/app.js
+++ .build/js/app.js
@@ -7,6 +7,7 @@ var userName;
 Viewer.prototype.getRepos = function(userName) {
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     for(i=0; i<response.length; i++) {
+      $("#results").append("<li>" + response[i].name + "</li>");
     console.log(response[i].name);
   };
   }).fail(function(error){
@@ -33,7 +34,7 @@ $(document).ready(function() {
   // };
   var user = "alandenison"
   viewer = new Viewer(user)
-  viewer.getRepos(user);
+ viewer.getRepos(user);
 
 });
 
--- js/viewer.js
+++ js/viewer.js
@@ -6,6 +6,7 @@ var userName;
 Viewer.prototype.getRepos = function(userName) {
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     for(i=0; i<response.length; i++) {
+      $("#results").append("<li>" + response[i].name + "</li>");
     console.log(response[i].name);
   };
   }).fail(function(error){
